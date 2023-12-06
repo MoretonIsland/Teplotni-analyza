c260adb751b702498fbb4d5e1ff5982b4ddae132
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2023-12-07 00:46:03 +0100

    Create temperatures.php

---
+++ temperatures.php
@@ -0,0 +1,91 @@
+<?php
+
+/*Asociativní pole $cityAbbreviations slouží k převodu zkratek měst na jejich celé názvy.*/
+$cityAbbreviations = [
+    'BR' => 'Brisbane',
+    'GC' => 'Gold Coast',
+    'SC' => 'Sunshine Coast',
+    'TSV' => 'Townsville',
+    'CR' => 'Cairns',
+    'GL' => 'Gladstone',
+    'KB' => 'Karumba',
+    'MI' => 'Mount Isa',
+    'EM' => 'Emerald',
+    'SG' => 'St.George',
+    'SE' => 'Seisia',
+    
+];
+ /* /* Asociativní pole teplot pro každý den ve formátu 'YYYY-MM-DD' a pro každé město. */
+   
+return array(
+    '2023-10-01' => array(
+        'BR' => 27, 'GC' => 27, 'SC' => 28,
+        'TSV' => 29, 'CR' => 29, 'GL' => 28, 'KB' => 36,
+        'MI' => 36, 'EM' => 32, 'SG' => 34,
+        'SE' => 31,
+    ),
+    '2023-10-02' => array(
+        'BR' => 28, 'GC' => 28, 'SC' => 26,
+        'TSV' => 29, 'CR' => 30, 'GL' => 30, 'KB' => 36,
+        'MI' => 37, 'EM' => 32, 'SG' => 35,
+        'SE' => 31,
+    ),
+    '2023-10-03' => array(
+        'BR' => 27, 'GC' => 27, 'SC' => 26,
+        'TSV' => 29, 'CR' => 31, 'GL' => 29, 'KB' => 34,
+        'MI' => 38, 'EM' => 32, 'SG' => 33,
+        'SE' => 31,
+
+    ),
+
+    '2023-10-04' => array(
+        'BR' => 26, 'GC' => 26, 'SC' => 26,
+        'TSV' => 29, 'CR' => 30, 'GL' => 27, 'KB' => 34,
+        'MI' => 31, 'EM' => 34, 'SG' => 32,
+        'SE' => 30,
+    ),
+    '2023-10-05' => array(
+        'BR' => 28, 'GC' => 28, 'SC' => 26,
+        'TSV' => 30, 'CR' => 32, 'GL' => 30, 'KB' => 37,
+        'MI' => 30, 'EM' => 33, 'SG' => 24,
+        'SE' => 31,
+    ),
+    '2023-10-06' => array(
+        'BR' => 26, 'GC' => 25, 'SC' => 25,
+        'TSV' => 30, 'CR' => 30, 'GL' => 29, 'KB' => 38,
+        'MI' => 32, 'EM' => 33, 'SG' => 25,
+        'SE' => 32,
+    ),
+    
+    
+    '2023-10-07' => array(
+        'BR' => 23, 'GC' => 22, 'SC' => 23,
+        'TSV' => 29, 'CR' => 31, 'GL' => 27, 'KB' => 36,
+        'MI' => 34, 'EM' => 31, 'SG' => 29,
+        'SE' => 32,
+    ),
+    '2023-10-08' => array(
+        'BR' => 23, 'GC' => 22, 'SC' => 23,
+        'TSV' => 30, 'CR' => 30, 'GL' => 26, 'KB' => 36,
+        'MI' => 36, 'EM' => 30, 'SG' => 28,
+        'SE' => 31,
+    ),
+    '2023-10-09' => array(
+        'BR' => 25, 'GC' => 23, 'SC' => 24,
+        'TSV' => 29, 'CR' => 28, 'GL' => 26, 'KB' => 36,
+        'MI' => 31, 'EM' => 30, 'SG' => 29,
+        'SE' => 31,
+    ),
+    
+    '2023-10-10' => array(
+        'BR' => 27, 'GC' => 26, 'SC' => 25,
+        'TSV' => 29, 'CR' => 30, 'GL' => 26, 'KB' => 40,
+        'MI' => 33, 'EM' => 31, 'SG' => 30,
+        'SE' => 31,
+    ),
+    
+    
+    
+);
+
+?>
