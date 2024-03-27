c70a1e703b701b216b08b8520e6ff08714a1aab0
MoretonIsland <107469969+MoretonIsland@users.noreply.github.com>
2024-03-27 01:06:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Teplotni analyza</title>
 </head>
 <body>
-
+<img src="Australie.png" alt="Obrázek Australie" style="width: 100%; height: auto; display: block;"
 <?php
 include 'CityTemperature.php';
 include 'TemperatureAnalyzer.php';
@@ -56,15 +56,14 @@ $highestOverallTemperature = $temperatureAnalyzer->highestOverallTemperature();
 
 ?>
 
-<!-- Prezentace výsledků teplotní analýzy na webové stránce -->
-
-<!-- Výsledky teplotní analýzy -->
-<h1>Temperature analysis from October 2023</h1>
-<p>City temperatures in the state of Queensland</p>
+// Výsledky teplotní analýzy na webové stránce -->
+<h1>October 2023 temperatures - Cities in Queensland</h1>
 
 <!-- Vkládání výsledků do kontejneru polem -->
 <div id="results">
 
+<hr id="results-hr">
+<br>
 <h2>Average temperature:</h2>
 <ul>
     <?php
@@ -79,7 +78,7 @@ $highestOverallTemperature = $temperatureAnalyzer->highestOverallTemperature();
         <li><?php echo $city->name . ': ' . number_format($city->averageTemperature(), 2) . ' °C'; ?></li>
     <?php endforeach; ?>
 </ul>
-
+<br>
 <h2>Highest temperature:</h2>
 <ul>
     <?php
@@ -95,21 +94,74 @@ $highestOverallTemperature = $temperatureAnalyzer->highestOverallTemperature();
 </ul>
 
 </div>
-
+<br>
 <!-- Celkový průměrný stav teplot, proměnná-->
 <h2>Overall average temperature:</h2>
 <p><?php echo ucfirst('overall average temperature: ' . number_format($overallAverageTemperature, 2)) . ' °C'; ?></p>
 
+<br>
 <!-- Nejvyšší zaznamenaná teplota ze všech měst, proměnná -->
 <h2>Highest recorded temperature from all cities:</h2>
 <p><?php echo ucfirst('highest recorded temperature from all cities: ' . number_format($highestOverallTemperature, 2)) . ' °C'; ?></p>
 
-<!-- Dodatečné informace -->
-<div id="additional-info">
-    <p>Temperature analysis from October 2023</p>
-    
+<br>
+<?php
+// index.php
+include 'SearchTemperatures.php';
+
+// Načtení dat
+$temperatures = include 'temperatures.php';
+
+// Zkratky měst
+$cityAbbreviations = [
+    'BR' => 'Brisbane',
+    'GC' => 'Gold Coast',
+    'SC' => 'Sunshine Coast',
+    'TSV' => 'Townsville',
+    'CR' => 'Cairns',
+    'GL' => 'Gladstone',
+    'KB' => 'Karumba',
+    'MI' => 'Mount Isa',
+    'EM' => 'Emerald',
+    'SG' => 'St.George',
+    'SE' => 'Seisia',
+];
+
+// Hledaná teplota
+$targetTemperature = 30;
+
+// Pole pro uchování nalezených výsledků
+$results = [];
+
+// Spuštění rekurzivního vyhledávání
+searchTemperature($temperatures, $targetTemperature, $results);
+
+
+// Výstup výsledků
+echo "<h2>Temperatures $targetTemperature °C and higher:</h2>";
+echo "<ul>";
+foreach ($results as $result) {
+    echo "<li>$result</li>";
+}
+echo "</ul>";
+?>
+
+
+<br>  
+<div class="image-grid">
+    <img src="Cairns.jpg" alt="Cairns">
+    <img src="Seisia.jpg" alt="Seisia">
+    <img src="Gladstone.jpg" alt="Gladstone">
+    <img src="Gladstone1.jpg" alt="Gladstone">
+    <img src="Bird.jpg" alt="Bird">
 </div>
 
+<br>
+<br>  
+ <!-- Dodatečné informace -->
+<div id="additional-info"> 
+<p>Temperature analysis - Queensland - October 2023</p>
+    
+</div>
 </body>
 </html>
-
